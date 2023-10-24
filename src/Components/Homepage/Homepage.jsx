ae2dd0699cfe42552375a685ef994bdc302a40a7
pbaki <pbaki999@gmail.com>
2023-10-24 19:14:20 +0200

    Homepage initial content with styling

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,11 +1,22 @@
-import "./Homepage.css";
 import { Link } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
+import "./Homepage.css";
 
 export default function Homepage() {
   return (
-    <div>
+    <div className="mainPage">
       <Navigation />
+      <div className="homeContent">
+        <h1>Welcome To Store Name</h1>
+        <p>
+          Lorem ipsum dolor sit amet. In velit debitis et eligendi molestiae vel
+          dolorem deleniti eum quia consequatur aut saepe dolorum qui inventore
+          dolores et maiores excepturi. Eum dolorem beatae id ipsam inventore id
+          odit internos aut molestias temporibus ab sint maxime qui quidem
+          optio.
+        </p>
+        <button className="shopButton">SHOP NOW</button>
+      </div>
     </div>
   );
 }
