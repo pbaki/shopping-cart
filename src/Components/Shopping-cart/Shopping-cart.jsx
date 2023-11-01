3533e1d6ff4524acb6482d7751f968f88a35ea22
pbaki <pbaki999@gmail.com>
2023-11-01 01:50:45 +0100

    Empty card component

--- src/Components/Shopping-cart/Shopping-cart.jsx
+++ src/Components/Shopping-cart/Shopping-cart.jsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
 import "./Shopping-cart.css";
 import Navigation from "../Navigation/Navigation";
+
 export default function ShoppingCart({ APIData }) {
   return (
     <div className="shoppingCartPage">
       <Navigation />
+      <EmptyCart />
+    </div>
+  );
+}
+
+function EmptyCart() {
+  return (
+    <div className="emptyCart">
+      <p>Cart is empty. Click button below to start shopping.</p>
+      <Link className="shopButton" to="/shop/1">
+        SHOP NOW
+      </Link>
     </div>
   );
 }
