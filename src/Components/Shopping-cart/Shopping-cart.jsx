80ed9af12752e0ba83060de0d82c697b1097d0b4
pbaki <pbaki999@gmail.com>
2023-11-07 01:09:23 +0100

    jsx for products in cart

--- src/Components/Shopping-cart/Shopping-cart.jsx
+++ src/Components/Shopping-cart/Shopping-cart.jsx
@@ -9,7 +9,41 @@ export default function ShoppingCart({ APIData, data }) {
   return (
     <div className="shoppingCartPage">
       <Navigation />
-      <EmptyCart />
+      {data.length > 0 ? <CartWithItems products={data} /> : <EmptyCart />}
+    </div>
+  );
+}
+function CartWithItems({ products }) {
+  function displayProducts() {
+    const productJSX = products.map((product) => {
+      return (
+        <div className="cartProduct" key={product.id}>
+          <button className="removeFromCart">X</button>
+          <div className="productImg">
+            <img className="img" src={product.image} alt="Product Image" />
+          </div>
+          <div className="productInfo">
+            <div className="productName">{product.title}</div>
+            <div className="quantityPrice">
+              <div className="howManyItems">
+                <div className="minusProduct">-</div>
+                <div className="quantity">{product.quantity}</div>
+                <div className="plusProduct">+</div>
+              </div>
+              <div className="price">{"$ " + product.price}</div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    return productJSX;
+  }
+  return (
+    <div className="cartItemsCheckout">
+      <div className="cartItems">
+        {products.length > 0 ? displayProducts() : null}
+      </div>
+      <div className="checkout"></div>
     </div>
   );
 }
