434633afce3936c725b61d33920b1e5fe11868ff
pbaki <pbaki999@gmail.com>
2023-11-28 18:50:48 +0100

    gh pages setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./main.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter basename="/shopping-cart">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
